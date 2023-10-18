cbf998307a3887781d234028519fd54eca7028e5
sheriff-gaye <sheriffgaye5@gmail.com>
2023-10-18 14:56:12 +0000

    all

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,4 +1,4 @@
-import { deleteUserById, getUsers } from '../db/users';
+import { deleteUserById, getUserById, getUsers } from '../db/users';
 import express from 'express';
 
 
@@ -34,3 +34,25 @@ export const deleteUser=async (reg:express.Request,res:express.Response)=>{
         
     }
 }
+
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+    try {
+      const { id } = req.params;
+      const { username } = req.body;
+  
+      if (!username) {
+        return res.sendStatus(400);
+      }
+  
+      const user = await getUserById(id);
+      
+      user.username = username;
+      await user.save();
+  
+      return res.status(200).json(user).end();
+    } catch (error) {
+      console.log(error);
+      return res.sendStatus(400);
+    }
+  }
--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { deleteUser, getAllUsers } from '../controllers/users';
-import { isOwner,isAuthenticated } from '../middleware/index';
-
+import { deleteUser, getAllUsers, updateUser } from '../controllers/users';
+import { isAuthenticated, isOwner } from '../middleware';
 
 export default (router:express.Router)=>{
     router.get('/users', isAuthenticated, getAllUsers);
-    router.delete('/users:id',isOwner,deleteUser)
+    router.delete('/users/:id',isOwner,deleteUser)
+    router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
+
 }
